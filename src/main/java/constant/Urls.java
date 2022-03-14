06ce9e127eb5db98adaa7d8096a1761cbbe117d4
QA-Alexandr-Lisovskij <lisovskijsanya0@mail.ru>
2022-03-14 13:20:09 +0300

    develop 14.03.2022

--- src/main/java/constant/Urls.java
+++
@@ -1,8 +0,0 @@
-package constant;
-
-
-
-public class Urls {
-    public static final String BASE_URL = "https://client.express-pay.by/";
-
-}
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -3,10 +3,10 @@ package model;
 import lombok.Builder;
 import lombok.Data;
 
-
 @Data
 @Builder
 public class User {
+
     private int id;
     private String login;
     private String password;
