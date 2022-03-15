412f465fdf55f853fabe5a7cac353a9d30c628d8
QA-Alexandr-Lisovskij <lisovskijsanya0@mail.ru>
2022-03-15 19:22:30 +0300

    develop 15.03.2022

--- src/test/java/tests/api/NegativeTest.java
+++ src/test/java/tests/api/NegativeTest.java
@@ -1,6 +1,27 @@
 package tests.api;
 
 import api.BaseApiTest;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.Test;
+
+
+import static api.EndPoints.GET_USER;
+import static io.restassured.RestAssured.given;
 
 public class NegativeTest extends BaseApiTest {
+
+    @Test
+    public void simpleApiTest(){
+        int id = 1;
+        given()
+                .pathParam("id",id)
+                .when()
+                .get(GET_USER)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK);
+
+    }
+
+
 }
--- src/test/java/tests/api/PositiveTest.java
+++ src/test/java/tests/api/PositiveTest.java
@@ -1,14 +1,20 @@
 package tests.api;
 
 import api.BaseApiTest;
+import io.restassured.mapper.ObjectMapperType;
+import io.restassured.response.Response;
+import model.User;
 import org.apache.http.HttpStatus;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-import static api.EndPoints.GET_ALL_USERS;
+import static api.EndPoints.*;
 import static io.restassured.RestAssured.given;
 
 public class PositiveTest extends BaseApiTest {
+
+    int id = 4;
     @Test
     public void simpleApiTest(){
         given()
@@ -18,5 +24,52 @@ public class PositiveTest extends BaseApiTest {
                 .log().body()
                 .statusCode(HttpStatus.SC_OK);
 
+    }
+
+    @Test
+    public void addProject3(){
+        User user = User.builder()
+                .login("FIFA")
+                .password("offal")
+                .build();
+
+        given()
+                .body(user, ObjectMapperType.GSON)
+                .when()
+                .post(ADD_USER)
+                .then().log().body()
+                .statusCode(HttpStatus.SC_CREATED);
+    }
+
+    @Test
+    public void updateProject(){
+
+
+        User user = User.builder()
+                .login("FIFOsu_upd")
+                .password("tytytyyty")
+                .build();
+
+        Response response =  given()
+                .pathParam("id",id)
+                .body(user, ObjectMapperType.GSON)
+                .when()
+                .post(UPDATE_USER)
+                .then()
+                .log().body()
+                .extract().response();
+
+        Assert.assertEquals(response.getBody().jsonPath().get("name"),user.getLogin());
+    }
+
+    @Test
+    public void deleteProject(){
+        given()
+                .pathParam("id", "3")
+                .when()
+                .post(DELETE_USER)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK);
     }
 }
